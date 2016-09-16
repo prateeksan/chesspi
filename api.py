654680fc974e8fcfc21b82b60e54304ed3d52713
Edmund <elam.txt@gmail.com>
2016-09-15 22:06:40 -0400

    Add sample chess pgn data

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
 # Python v3.5.1
 from flask import Flask
 from flask_restful import Resource, Api, abort
+import pgn
 
 app = Flask(__name__)
 api = Api(app)
 
-games = ["Game 0", "Game 1", "Game 2"]
+# games = ["Game 0", "Game 1", "Game 2"]
+
+games_input = open('sample_data/kasparov.pgn').read()
+games = pgn.loads(games_input)
+# games = [pgn.dumps(g) for g in games_models]
 
 # Abort the request if a game cannot be found
 def abort_if_game_doesnt_exist(game_id):
@@ -23,14 +28,14 @@ class Game(Resource):
     def get(self, game_id):
         game_id = int(game_id)
         abort_if_game_doesnt_exist(game_id)
-        return games[game_id]
+        return {'pgn': pgn.dumps(games[game_id])}
 
 
 # GameList
 # Shows a list of all games
 class GameList(Resource):
     def get(self):
-        return games
+        return [{'pgn': pgn.dumps(g)} for g in games]
 
 # API Routing
 api.add_resource(HelloWorld, '/')
