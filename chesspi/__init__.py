003fda0aa82ed0ec24fa389b6c00b4bf9ab108be
Edmund <elam.txt@gmail.com>
2016-09-18 11:42:21 -0400

    Add /players and /eco_code routes
    - Player and EcoCode resources currently read from the games list
    - Also update sample calls on home page

--- chesspi/__init__.py
+++ chesspi/__init__.py
@@ -9,6 +9,8 @@ from chesspi.common.models import games
 # import resources
 from chesspi.resources.games import Game, GameList
 from chesspi.resources.index import Index
+from chesspi.resources.eco_codes import EcoCodeList
+from chesspi.resources.players import PlayerList
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,3 +19,5 @@ api = Api(app)
 api.add_resource(Index, '/')
 api.add_resource(Game, '/games/<game_id>')
 api.add_resource(GameList, '/games')
+api.add_resource(EcoCodeList, '/eco_codes')
+api.add_resource(PlayerList, '/players')
--- chesspi/resources/index.py
+++ chesspi/resources/index.py
@@ -5,8 +5,29 @@ class Index(Resource):
         return {
             'chesspi': 'Chess API',
             'sample_calls': [
-                '/games',
-                '/games/16',
-                '/games?name=chandler',
-                '/games?eco=b22']
+                {
+                    'url': '/games',
+                    'description': 'List of all games in the DB'
+                },
+                {
+                    'url': '/games/16',
+                    'description': 'Show game with id 16'
+                },
+                {
+                    'url': '/games?name=anand',
+                    'description': 'Show games played by Anand'
+                },
+                {
+                    'url': '/games?eco=b22',
+                    'description': 'Show games opening with ECO B22'
+                },
+                {
+                    'url': '/players',
+                    'description': 'List of all players in DB'
+                },
+                {
+                    'url': '/eco_codes',
+                    'description': 'List of all eco codes in DB'
+                }
+            ]
         }
