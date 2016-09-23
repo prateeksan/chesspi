55b9192e009e00906d888ed1645714c80ce51575
Edmund <elam.txt@gmail.com>
2016-09-22 22:03:10 -0400

    Add rate limits to api
    - Global rate limits are set in config.py.
    - Currently the limits are set to 2 requests per second and 100/minute
    - Added a custom 1/second rate limit for the GameList resource
    - Updated requirements.txt
    - Reorganized __init__.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# Flask Limiter configs
+RATELIMIT_STRATEGY = 'fixed-window'
+RATELIMIT_GLOBAL = '2/second,100/minute'
