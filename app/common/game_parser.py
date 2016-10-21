756bebb407ae42d34aa65716aa4ebb77e9f9fb85
prateeksan <sanyal.prateek@gmail.com>
2016-10-20 22:16:11 -0400

    wip: working on skeleton for GameParser:
    - Private methods added to parse and update db with players and pairings
    - Private methods called in add_games and __add_game methods
    - Public method player_in_db added that checks if player is in db

--- app/common/game_parser.py
+++ app/common/game_parser.py
@@ -19,7 +19,19 @@ class GameParser:
 
         if self.parsed_games:
             for game in parsed_games:
-                self.__add_game(game)
+                # Returns dict like: {white: <id>, black: <id>}
+                db_player_ids = self.__add_players(white=game.white, 
+                                                    black=game.black)
+                db_game_id = self.__add_game(game)
+                self.__add_pairings(game_id=db_game_id,
+                                    player_ids=db_player_ids)
+
+    def player_in_db(player):
+        """Takes a player name and checks if player in db
+        If present, it returns the player id,
+        else returns None"""
+
+        # TODO(complete this)
 
     def __add_game(self, game):
         """Takes a single game object and adds it to the Game table in the db"""
@@ -37,3 +49,45 @@ class GameParser:
             )
         db.session.add(db_game)
         db.session.commit()
+        return db_game.id
+
+    def __add_players(self, white, black):
+        """Adds players to db if players not in db.
+        Returns dict like {'white': <id>, 'black': <id>}"""
+
+        white_parsed = self.__parse_player_name(white)
+        black_parsed = self.__parse_player_name(black)
+
+        white_id = self.player_in_db(white_parsed)
+        black_id = self.player_in_db(black_parsed)
+
+        if not white_id:
+            db_white = models.Player(
+                    first_name= white_parsed['first_name'],
+                    middle_name = white_parsed['middle_name'],
+                    last_name = white_parsed['last_name']
+                    )
+
+        if not black_id:
+            db_black = models.Player(
+                    first_name= black_parsed['first_name'],
+                    middle_name = black_parsed['middle_name'],
+                    last_name = black_parsed['last_name']
+                    )
+
+        return {'white': white_id, 'black': black_id}
+
+
+    def __parse_player_name(self, name_string):
+        """Takes a player name string and returns a dict as:
+        {'first_name': <string>, 'middle_name': <string>, last_name': <string>}
+        """
+        name_dict = {}
+
+        # TODO(complete this)
+
+    def __add_parings(self, game_id, player_id):
+        """Receives a game_id and a dict with player ids,
+        adds two pairings for white and black"""
+
+        # TODO(complete this)
