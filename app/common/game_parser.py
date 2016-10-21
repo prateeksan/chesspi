9e6c7637de7c7e1d558a246a9ce34c163d4abdb4
prateeksan <sanyal.prateek@gmail.com>
2016-10-20 21:50:08 -0400

    wip: added add_games method and __add_game private method:
    + add_games calls __add_game for each game which adds game to db
    + todo: needs testing
    + todo: needs a method to populate players and pairings

--- app/common/game_parser.py
+++ app/common/game_parser.py
@@ -1,3 +1,6 @@
+import pgn
+from app import db, models
+
 class GameParser:
     """Init with a pgn to parse into an object that is db ready.
     Populate the Game table with parsed games.
@@ -9,5 +12,28 @@ class GameParser:
 
         self.pgn = pgn
         self.game_id = game_id
-        self.parsed_games = []
+        self.parsed_games = pgn.loads(self.pgn) if self.pgn else None
 
+    def add_games(self):
+        """If pgn was provided and parsed, adds games from pgn to the db"""
+
+        if self.parsed_games:
+            for game in parsed_games:
+                self.__add_game(game)
+
+    def __add_game(self, game):
+        """Takes a single game object and adds it to the Game table in the db"""
+
+        moves_string = (',').join(game.moves)
+        db_game = models.Game(
+                event=game.event,
+                site=game.site,
+                date=game.date,
+                match_round=game.round,
+                result=game.result,
+                white_elo=game.whiteelo,
+                black_elo=game.blackelo,
+                moves=game.moves
+            )
+        db.session.add(db_game)
+        db.session.commit()
