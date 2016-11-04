9fec6c81363b68cd85d5b7c5cf8d4dacfd9a9aa2
prateeksan <sanyal.prateek@gmail.com>
2016-11-03 23:08:52 -0400

    added ModelTests class with setUp and tearDown methods

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -11,3 +11,17 @@ from sample_data.games_string import SAMPLE_GAMES_STRING
 
 from app import app, db
 from app import models
+
+class ModelTests(unittest.TestCase):
+  """This class tests all models"""
+  
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
