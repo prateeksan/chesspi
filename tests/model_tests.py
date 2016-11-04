10d8aad55f016c1333c6f50a0ecc09f0e6df2d46
prateeksan <sanyal.prateek@gmail.com>
2016-11-03 23:06:28 -0400

    added requisite imports to model_tests

---
+++ tests/model_tests.py
@@ -0,0 +1,13 @@
+#!flask/bin/python
+import os
+import unittest
+import sys
+import pgn
+# Set path to parent directory
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from config import basedir
+from sample_data.games_string import SAMPLE_GAMES_STRING
+
+from app import app, db
+from app import models
