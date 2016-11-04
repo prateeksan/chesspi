aa8656725850bd2608d717f95c0699428e717a9d
prateeksan <sanyal.prateek@gmail.com>
2016-11-03 23:36:02 -0400

    fixed indentation, added SAMPLE_GAMES global constant

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -12,55 +12,58 @@ from sample_data.games_string import SAMPLE_GAMES_STRING
 from app import app, db
 from app import models
 
+SAMPLE_GAMES = pgn.loads(SAMPLE_GAMES_STRING)
+print(SAMPLE_GAMES)
+
 class ModelTests(unittest.TestCase):
   """This class tests all models"""
 
-    def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
-        self.app = app.test_client()
-        db.create_all()
+  def setUp(self):
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    self.app = app.test_client()
+    db.create_all()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+  def tearDown(self):
+    db.session.remove()
+    db.drop_all()
 
-    def test_create_game(self):
-      """Create a Game entry in db"""
+  def test_create_game(self):
+    """Create a Game entry in db"""
 
-    def test_read_game(self):
-      """Query a game by id and read its fields"""
+  def test_read_game(self):
+    """Query a game by id and read its fields"""
 
-    def test_update_game(self):
-      """Update fields of a game in db"""
+  def test_update_game(self):
+    """Update fields of a game in db"""
 
-    def test_delete_game(self):
-      """Delete game entry from db with game id"""
+  def test_delete_game(self):
+    """Delete game entry from db with game id"""
 
-    def test_create_player(self):
-      """Create a player entry in db"""
+  def test_create_player(self):
+    """Create a player entry in db"""
 
-    def test_read_player(self):
-      """Query a player by id and read its fields"""
+  def test_read_player(self):
+    """Query a player by id and read its fields"""
 
-    def test_update_player(self):
-      """Update fields of a player in db"""
+  def test_update_player(self):
+    """Update fields of a player in db"""
 
-    def test_delete_player(self):
-      """Delete player entry from db with game id"""
+  def test_delete_player(self):
+    """Delete player entry from db with game id"""
 
-    def test_player_full_name(self):
-      """Test Player.full_name method"""
+  def test_player_full_name(self):
+    """Test Player.full_name method"""
 
-    def test_create_pairing(self):
-      """Create a pairing entry in db"""
+  def test_create_pairing(self):
+    """Create a pairing entry in db"""
 
-    def test_pairing_backrefs(self):
-      """Check backrefs for both Player and Game in a pairing"""
+  def test_pairing_backrefs(self):
+    """Check backrefs for both Player and Game in a pairing"""
 
-    def test_delete_pairing(self):
-      """Delete a pairing from the db"""
+  def test_delete_pairing(self):
+    """Delete a pairing from the db"""
 
-  if __name__ == '__main__':
-    unittest.main()
+if __name__ == '__main__':
+  unittest.main()
