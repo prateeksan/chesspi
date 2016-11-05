8cf69fc3a6dc107eef4a350bf5c507cda9647b07
prateeksan <sanyal.prateek@gmail.com>
2016-11-04 21:46:08 -0400

    test_delete_player complete, db.session.commit() commands removed

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -59,7 +59,6 @@ class ModelTests(unittest.TestCase):
     game_from_db = models.Game.query.get(1)
     game_from_db.site = 'Mars'
     db.session.add(game_from_db)
-    db.session.commit()
     # Read game from db again
     reread_game = models.Game.query.get(1)
     assert reread_game.site == 'Mars'
@@ -68,7 +67,6 @@ class ModelTests(unittest.TestCase):
     """Delete game entry from db with game id"""
     game = self.__create_test_game()
     models.Game.query.filter_by(id=1).delete()
-    db.session.commit()
     games_count = len(models.Game.query.all())
     assert games_count == 0
 
@@ -77,20 +75,25 @@ class ModelTests(unittest.TestCase):
     player = self.__create_test_player()
     assert player == models.Player.query.get(1)
 
+  def test_player_full_name(self):
+    """Test Player.full_name method"""
+    player = self.__create_test_player()
+    assert player.full_name() == 'Carlsen, Magnus'
+
   def test_update_player(self):
     """Update fields of a player in db"""
     player = self.__create_test_player()
     player.first_name = 'Test'
     db.session.add(player)
-    db.session.commit()
     reread_player = models.Player.query.get(1)
     assert reread_player.first_name == 'Test'
 
   def test_delete_player(self):
     """Delete player entry from db with game id"""
-
-  def test_player_full_name(self):
-    """Test Player.full_name method"""
+    player = self.__create_test_player()
+    models.Player.query.filter_by(id=1).delete()
+    players_count = len(models.Player.query.all())
+    assert players_count == 0
 
   def test_create_pairing(self):
     """Create a pairing entry in db"""
@@ -115,18 +118,15 @@ class ModelTests(unittest.TestCase):
       moves=(",").join(sample_game.moves),
       )
     db.session.add(game)
-    db.session.commit()
     return game
 
   def __create_test_player(self):
     """Creates a test player in db"""
     player = models.Player(
       first_name='Magnus',
-      middle_name='M',
       last_name='Carlsen'
       )
     db.session.add(player)
-    db.session.commit()
     return player
 
 if __name__ == '__main__':
